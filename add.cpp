992d7a3a0217d7ea890b15432a5dc1a5406c6ddd
adithyalaxmiprasad <adithya.l386@gmail.com>
2024-12-18 14:21:34 +0530

    this is a cpp file

---
+++ add.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,total;
+
+    cout <<"give the numbers for a & b";
+    cin >> a ;
+    cin >>b;
+
+    total= a+b;
+
+    cout << "toal of a and b"<<total<<endl;
+}
